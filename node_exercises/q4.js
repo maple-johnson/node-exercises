2484238cc46997c9cea28818077b97ad429c2fcc
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-14 15:31:57 -0800

    Completed q4

--- node_exercises/q4.js
+++ node_exercises/q4.js
@@ -2,8 +2,24 @@
 A palindrome is a word, number, or other sequence of characters that reads the same forward and backward (assume there will be no spaces, tabs or punctuation).
 */
 
-function checkPalindrome(textInput) {
+function checkPalindrome(textInput) 
+{
+     let reverseString = "";
     
+     for(let i = textInput.length; i > 0; i--)
+    {
+          reverseString += textInput.charAt(i -1);
+    }
+
+    if(reverseString === textInput)
+    {
+          return true;
+    }
+    else
+    {
+          return false;
+    }
+
 }
 
 // Test for palindrome
@@ -12,3 +28,5 @@ let testString = "racecar"; // true
 let testString2 = "hello"; // false
 
 // TODO: display the result of the test strings in the console
+console.log(checkPalindrome(testString));
+console.log(checkPalindrome(testString2));
