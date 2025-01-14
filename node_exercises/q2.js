793a4e785f28b260ca3302166d88d966baa5a1ea
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-14 14:56:47 -0800

    Completed q2

--- node_exercises/q2.js
+++ node_exercises/q2.js
@@ -1,5 +1,17 @@
 // 2. Find the largest number in an array (using a loop)
-function findLargest(array) {
+function findLargest(array) 
+{
+     let bigNum = 0;
+
+     for(let element of array)
+     {
+          if (element > bigNum)
+          {
+               bigNum  = element;
+          }
+     }
+
+     return bigNum;
 
 }
 
@@ -7,3 +19,4 @@ function findLargest(array) {
 let testArray2 = [1, 4, 5, 4, 3]; // 5
 
 // TODO: display the largest number in the array in the console
+console.log(findLargest(testArray2));
